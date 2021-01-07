9b9d7bcb25028f7f9562723978cd123b48780e07
Alexander Varwijk <alexander@getopensocial.com>
2021-01-07 20:25:30 +0100

    Use 0.19 instead of 0.20
    Apparently 0.20 already contained the change that caused `msguniq` to
    choke on duplicates. 0.19 is a version that works with OSTE.

--- Formula/gettext@0.19.rb
+++ Formula/gettext@0.19.rb
@@ -1,13 +1,12 @@
-class GettextAT020 < Formula
+class GettextAT019 < Formula
   desc "GNU internationalization (i18n) and localization (l10n) library"
   homepage "https://www.gnu.org/software/gettext/"
-  url "https://ftp.gnu.org/gnu/gettext/gettext-0.20.2.tar.xz"
-  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.20.2.tar.xz"
-  sha256 "b22b818e644c37f6e3d1643a1943c32c3a9bff726d601e53047d2682019ceaba"
-  license "GPL-3.0"
-  revision 1
+  url "https://ftp.gnu.org/gnu/gettext/gettext-0.19.8.1.tar.xz"
+  mirror "https://ftpmirror.gnu.org/gettext/gettext-0.19.8.1.tar.xz"
+  sha256 "105556dbc5c3fbbc2aa0edb46d22d055748b6f5c7cd7a8d99f8e7eb84e938be4"
 
-  uses_from_macos "ncurses"
+  keg_only :shadowed_by_macos,
+    "macOS provides the BSD gettext library & some software gets confused if both are in the library path"
 
   def install
     system "./configure", "--disable-dependency-tracking",
@@ -15,8 +14,6 @@ class GettextAT020 < Formula
                           "--disable-debug",
                           "--prefix=#{prefix}",
                           "--with-included-gettext",
-                          # Work around a gnulib issue with macOS Catalina
-                          "gl_cv_func_ftello_works=yes",
                           "--with-included-glib",
                           "--with-included-libcroco",
                           "--with-included-libunistring",
